2e5081b13a1d371781e79655d0b56c2957a6fa06
Dorukhan Bekdur <dorukhanbekdur@gmail.com>
2025-07-17 22:19:33 +0300

    Add C++ solution: Input and Output

---
+++ C++/Introduction/input-and-output.cpp
@@ -0,0 +1,21 @@
+// Language: C++
+// Category: C++ > Introduction
+// Problem: Input and Output
+// Author: Dorukhan Bekdur
+
+#include <iostream> // For standard input and output
+
+using namespace std;
+
+int main() 
+{
+    int a, b, c;
+    
+    // Read three integers from standard input
+    cin >> a >> b >> c;
+    
+    // Output their sum
+    cout << a + b + c << endl;
+    
+    return 0; // Exit the program
+}
