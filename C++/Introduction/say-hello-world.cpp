a4ff8179e256b54cbf9815236464e572130d0bc5
Dorukhan Bekdur <dorukhanbekdur@gmail.com>
2025-07-17 22:11:16 +0300

    Initial commit with folder structure and first C++ solution

---
+++ C++/Introduction/say-hello-world.cpp
@@ -0,0 +1,15 @@
+// Language: C++
+// Category: C++ > Introduction
+// Problem: Say "Hello, World!" With C++
+// Author: Dorukhan Bekdur
+
+#include <iostream> // Required for input/output operations
+
+using namespace std;
+
+int main() {
+    // Output the string "Hello, World!" followed by a newline
+    cout << "Hello, World!" << endl;
+    
+    return 0; // Exit the program
+}
